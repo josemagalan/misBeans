092545e0efb603c16156f57dd82e8cdb3d156aa4
Smarquina <sergyzen@gmail.com>
2015-12-29 13:59:02 +0100

    Octava iteración
    - Añadido de sql para importar la base de datos

--- src/BaseBundle/Controller/PartidaController.php
+++ src/BaseBundle/Controller/PartidaController.php
@@ -47,6 +47,7 @@ class PartidaController extends Controller
         $user = $this->getUser();
         $user_id = $user->getId();
         $gravatar = $this->getGravatar($user->getEmail());
+        /** @var ObjectManager $em */
         $em = $this->getDoctrine()->getManager();
         $router = $this->container->get('router');
 
